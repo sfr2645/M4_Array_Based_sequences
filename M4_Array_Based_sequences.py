358d55011c61b1513b78bfba16d711b43b11eab2
sfr2645 <sfr2645@email.vccs.edu>
2026-04-21 16:59:09 -0400

    corrected class name

--- M4_Array_Based_sequences.py
+++ M4_Array_Based_sequences.py
@@ -13,7 +13,7 @@ class Stack:
     def peek(self):
         if not self.is_empty():
             return self.items[-1]
-        raise IndexError("Peek from empty stack")
+        raise IndexError("Peek from empty stack.")
 
     def is_empty(self):
         return len(self.items) == 0
