3edc47bb7d922cefa5161512ad7870576ca2a0ad
sfr2645 <sfr2645@email.vccs.edu>
2026-04-21 17:28:23 -0400

    add infix

---
+++ infix_converter.py
@@ -0,0 +1,35 @@
+from stack import Stack
+
+class InfixToPostfixConverter:
+    def __init__(self, expression):
+        self.expression = expression
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2}
+
+    def convert(self):
+        stack = Stack()
+        output = []
+        tokens = self.expression.split()
+
+        for token in tokens:
+            if token.isalnum():
+                output.append(token)
+
+            elif token == '(':
+                stack.push(token)
+
+            elif token == ')':
+                while not stack.is_empty() and stack.peek() != '(':
+                    output.append(stack.pop())
+                stack.pop()
+
+            else:
+                while (not stack.is_empty() and
+                       stack.peek() != '(' and
+                       self.precedence[stack.peek()] >= self.precedence[token]):
+                    output.append(stack.pop())
+                stack.push(token)
+
+        while not stack.is_empty():
+            output.append(stack.pop())
+
+        return " ".join(output)
